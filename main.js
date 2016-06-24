45f7e8023da9664ac39ec66aa4df1ce0fa8742b9
Trevor Norris <trev.norris@gmail.com>
2016-06-23 21:11:53 -0600

    Initial commit
    Moving things over from another repository for sanity.

---
+++ main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+// Retrieve all commendations for a Spartan Company.
+Object.assign(module.exports, require('./lib/commendations'));
+
+// Retrieve list of members in a Spartan Company.
+Object.assign(module.exports, require('./lib/members'));
+
+// Retrieve Arena Service Record for a list of Spartans.
+Object.assign(module.exports, require('./lib/arena-service-record'));
+
+// Retrieve Warzone Service Record for a list of Spartans.
+Object.assign(module.exports, require('./lib/warzone-service-record'));
